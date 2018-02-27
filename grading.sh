d4d230bda6e5b326e08e9b159b73d5cf0f8bb41d
Carrie Coxwell <carrie.coxwell@gmail.com>
2018-02-27 11:24:47 -0600

    updated chat_modules

--- grading.sh
+++ grading.sh
@@ -3,14 +3,18 @@
 url=$1
 project=$2
 commit=$3
+extension=".git"
+full_url=$url$extension
 modules="_modules/node_modules"
 mods_folder=$project$modules
+grading_folder="~/dev/bloc/grading/"
 rm -rf $project
 echo "deleting existing $jams folder"
-git clone $url $project
+git clone $full_url $project
 echo "copying node_modules into project folder"
 cp -R "$mods_folder" "$project"
 cd "$project"
+subl $grading_folder$project
 if [ -n "$commit" ]; then
   echo "checking out $commit"
   git checkout "$commit"
